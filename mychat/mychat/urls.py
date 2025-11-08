703b9b05ca4551596a33197986606b30c6ed05a2
Takib Khan <c213101@ugrad.iiuc.ac.bd>
2025-11-08 14:22:42 +0600

    Fix favicon 400 error

--- mychat/mychat/urls.py
+++ mychat/mychat/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
+    path('', include('base.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/images/icon.jpg')),
 ]
