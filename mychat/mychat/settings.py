f6deb46828666172d0ca3699d7b58a33e28d0080
Takib Khan <c213101@ugrad.iiuc.ac.bd>
2025-11-08 14:30:12 +0600

    Add RENDER_EXTERNAL_HOSTNAME to ALLOWED_HOSTS

--- mychat/mychat/settings.py
+++ mychat/mychat/settings.py
@@ -28,6 +28,11 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
+# Allow Render.com hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
